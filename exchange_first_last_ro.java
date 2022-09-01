0e93bf044afa41e132587622a372ca5ed30a0fc2
harinimohanraj <84395251+harinim7523@users.noreply.github.com>
2022-09-01 17:42:10 +0530

    Create exchange_first_last_ro.java

---
+++ exchange_first_last_ro.java
@@ -0,0 +1,45 @@
+package matrix;
+
+import java.util.Scanner;
+
+public class exchange_first_last_ro {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of the array");
+		int n=sc.nextInt();
+		int a[][]=new int[n][n];
+		System.out.println("Enter the array elements");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				a[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println("The array is:");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		for(int i=0;i<n;i++)
+		{
+			int temp=a[0][i];
+			a[0][i]=a[n-1][i];
+			a[n-1][i]=temp;
+		}
+		System.out.println("The array after modification is:");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
