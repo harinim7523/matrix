8402874b4fb88641a25b0592bea3bc3e992c4a2d
harinimohanraj <84395251+harinim7523@users.noreply.github.com>
2022-09-06 10:44:52 +0530

    Create rotate_by_90_anti.java

---
+++ rotate_by_90_anti.java
@@ -0,0 +1,58 @@
+package matrix;
+
+import java.util.Scanner;
+
+public class rotate_by_90_anti {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of the array");
+		int n=sc.nextInt();
+		int a[][]=new int[n][n];
+		System.out.println("Enter the array elements");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				a[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println("The original array is:");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i;j<n;j++)
+			{
+				int temp=a[i][j];
+				a[i][j]=a[j][i];
+				a[j][i]=temp;
+			}
+		}
+		int l=0,e=n-1;
+		for(int i=0;i<n;i++)
+		{
+		for(l=0,e=n-1;l<e;l++,e--)
+		{
+			int temp=a[l][i];
+			a[l][i]=a[e][i];
+			a[e][i]=temp;
+		}
+		}
+		System.out.println("The modified array is:");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
