c84fe8b935631dc93d83d64815bc4e5fb9c58f60
harinimohanraj <84395251+harinim7523@users.noreply.github.com>
2022-09-26 19:35:41 +0530

    Create  swap_major_minor_diagonals.java

---
+++ swap_major_minor_diagonals.java
@@ -0,0 +1,54 @@
+package matrix;
+
+import java.util.Scanner;
+//Swap the major and the minor diagonals
+public class swap_major_minor_diagonals {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of the array");
+		int n=sc.nextInt();
+		int a[][]=new int[n][n];
+		System.out.println("Enter the array elements");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				a[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println("The array is:");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		int temp=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==j)
+				{
+					temp=a[i][j];
+					a[i][j]=a[i][n-i-1];
+					a[i][n-i-1]=temp;
+				}
+			}
+			
+		}
+		System.out.println("The changed matrix is");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
